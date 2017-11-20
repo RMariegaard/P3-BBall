e13f04549b79cdd35b0a1cc56bd3d23258a2cbe3
casper-lp <cpede16@student.aau.dk>
2017-11-20 14:13:03 +0100

    Created schedule and shift class

---
+++ VolunteerSystem_Serious/VolunteerSystem/VolunteerSystem/Schedule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolunteerSystem
+{
+    public class Schedule
+    {
+        private int _id;
+        private int _year;
+        private List<Shift> _shifts;
+
+        public void CreateNewShift(DateTime time, string task, int volunteersNeeded)
+        {
+            _shifts.Add(new Shift(time, task, volunteersNeeded));
+        }
+
+        public Schedule(int year)
+        {
+            //Assign ID??
+            throw new NotImplementedException();
+            _shifts = new List<Shift>();
+        }
+
+
+    }
+}
---
+++ VolunteerSystem_Serious/VolunteerSystem/VolunteerSystem/Shift.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolunteerSystem
+{
+    public class Shift
+    {
+        private DateTime _time;
+        private string _task;
+        private int _volunteersNeeded;
+        private List<Worker> _workers;
+        private List<Request> _requests;
+        private string _description;
+
+        public bool IsFilled() => NumberOfVolunteers() >= _volunteersNeeded;
+        public int NumberOfVolunteers() => _workers.Count;
+        public int NumberOfRequests() => _requests.Count;
+
+ 
+
+
+        public Shift(DateTime time, string task, int volunteersNeeded, string description)
+        {
+            this._time = time;
+            this._task = task;
+            this._volunteersNeeded = volunteersNeeded;
+            this._description = description;
+            _workers = new List<Worker>();
+            _requests = new List<Request>();
+        }
+
+        public void EditShift()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveWorker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateRequest()
+        {
+            _requests.Add(new Request());
+        }
+
+        public string GetInformation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
