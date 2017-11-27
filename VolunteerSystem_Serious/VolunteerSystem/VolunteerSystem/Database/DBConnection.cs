1a5718d1d90fa3978f2bf68abcf3ca0559218a1b
casper-lp <cpede16@student.aau.dk>
2017-11-27 17:21:48 +0100

    Det ville gøre det hele nemmere
    hvis vi kunne få linq SQL til at virke

--- VolunteerSystem_Serious/VolunteerSystem/VolunteerSystem/Database/DBConnection.cs
+++ VolunteerSystem_Serious/VolunteerSystem/VolunteerSystem/Database/DBConnection.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 using System.Data.Sql;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
-
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace VolunteerSystem.Database
 {
     public class DBConnection
     {
+
 
         private static string _connectionString = "Server=tcp:hackermarksdisciple.database.windows.net,1433;Initial Catalog=P3-Skovbakken;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
@@ -19,6 +21,9 @@ namespace VolunteerSystem.Database
 
 
         public static SqlConnection Connection { get { return _sqlConnection; } }
+
+        private static DataContext dataContext = new DataContext(_sqlConnection);
+        public static DataContext DataContext { get { return dataContext; } }
 
     }
 }
