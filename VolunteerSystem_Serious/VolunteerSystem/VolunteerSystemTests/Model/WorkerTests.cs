cd5114ec7c66ac69730a5da186d39a6bda309919
Jackilans <mark1904@hotmail.dk>
2017-12-02 13:47:05 +0100

    UNITTEST hele shift klassen er tested

---
+++ VolunteerSystem_Serious/VolunteerSystem/VolunteerSystemTests/Model/WorkerTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using VolunteerSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolunteerSystem.Tests
+{
+    [TestFixture()]
+    public class WorkerTests
+    {
+
+        //idk tror ikke denne klasse skal testes overhoved
+        [TestCase("Peter", "mark@test.dk", "Peter\tmark@test.dk")]
+        [TestCase("Jens", "jens@test.dk", "Jens\tjens@test.dk")]
+        public void GetInformationTest(string name, string email, string actual)
+        {
+            Worker worker = new Volunteer(name, email, "");
+
+            Assert.AreEqual(worker.GetInformation(), actual);
+        }
+
+
+    }
+}
