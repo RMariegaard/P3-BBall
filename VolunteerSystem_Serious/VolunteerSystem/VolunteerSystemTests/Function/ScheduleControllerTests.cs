d21fb64d86871597dcb656434c4574bf60176f05
Jackilans <mark1904@hotmail.dk>
2017-12-04 12:58:34 +0100

    comment

--- VolunteerSystem_Serious/VolunteerSystem/VolunteerSystemTests/Function/ScheduleControllerTests.cs
+++ VolunteerSystem_Serious/VolunteerSystem/VolunteerSystemTests/Function/ScheduleControllerTests.cs
@@ -31,118 +31,118 @@ namespace VolunteerSystem.Tests
             Assert.AreEqual(controller.ScheduleYear(), schedule.Year);
         }
 
-        [Test()]
-        public void GetAllRequestsTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetAllRequestsTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetAllShiftsTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetAllShiftsTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetAllTasksTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetAllTasksTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void CreateShiftTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void CreateShiftTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void CreateTaskTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void CreateTaskTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void RemoveTaskTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void RemoveTaskTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void EditShiftTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void EditShiftTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void DeleteShiftTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void DeleteShiftTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void AddWorkerToShiftTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void AddWorkerToShiftTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void RemoveWorkerFromShiftTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void RemoveWorkerFromShiftTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void RemoveWorkerFromAllHisShiftsTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void RemoveWorkerFromAllHisShiftsTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void RemoveAllRequestsForAWorkerTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void RemoveAllRequestsForAWorkerTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void RemoveRequestTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void RemoveRequestTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void ViewShiftInformationTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void ViewShiftInformationTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void ViewMyShiftsTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void ViewMyShiftsTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void SendRequestTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void SendRequestTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void ApproveRequestTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void ApproveRequestTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void DenyRequestTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void DenyRequestTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void FindSingleShiftTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void FindSingleShiftTest()
+    //    {
+    //        Assert.Fail();
+    //    }
     }
 }
