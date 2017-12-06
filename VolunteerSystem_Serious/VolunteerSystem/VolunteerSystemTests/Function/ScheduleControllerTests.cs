50d8afed091e51464dc6c1037cc1cbdd9d2450e1
Jackilans <mark1904@hotmail.dk>
2017-12-06 15:34:24 +0100

    few tests

--- VolunteerSystem_Serious/VolunteerSystem/VolunteerSystemTests/Function/ScheduleControllerTests.cs
+++ VolunteerSystem_Serious/VolunteerSystem/VolunteerSystemTests/Function/ScheduleControllerTests.cs
@@ -31,118 +31,132 @@ namespace VolunteerSystem.Tests
             Assert.AreEqual(controller.ScheduleYear(), schedule.Year);
         }
 
-    //    [Test()]
-    //    public void GetAllRequestsTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void GetAllRequestsTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void GetAllShiftsTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void GetAllShiftsTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void GetAllTasksTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void GetAllTasksTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void CreateShiftTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        [Test()]
+        public void CreateShiftTest()
+        {
+            Schedule schedule = new Schedule(2018);
+            ScheduleController scheduleController = new ScheduleController(schedule);
+            Shift shift = new Shift(new DateTime(), new DateTime(), "taski", 9, "test");
 
-    //    [Test()]
-    //    public void CreateTaskTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+            scheduleController.CreateShift(shift);
+            Assert.IsTrue(schedule.Shifts.Contains(shift));
 
-    //    [Test()]
-    //    public void RemoveTaskTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        }
 
-    //    [Test()]
-    //    public void EditShiftTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void CreateTaskTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void DeleteShiftTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void RemoveTaskTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void AddWorkerToShiftTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void EditShiftTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void RemoveWorkerFromShiftTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        [Test()]
+        public void DeleteShiftTest()
+        {
+            Schedule schedule = new Schedule(2018);
+            ScheduleController scheduleController = new ScheduleController(schedule);
+            Shift shift = new Shift(new DateTime(), new DateTime(), "taski", 9, "test");
+            schedule.Shifts.Add(shift);
 
-    //    [Test()]
-    //    public void RemoveWorkerFromAllHisShiftsTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+            scheduleController.DeleteShift(shift);
 
-    //    [Test()]
-    //    public void RemoveAllRequestsForAWorkerTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+            Assert.IsFalse(schedule.Shifts.Contains(shift));
 
-    //    [Test()]
-    //    public void RemoveRequestTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        }
 
-    //    [Test()]
-    //    public void ViewShiftInformationTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void AddWorkerToShiftTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void ViewMyShiftsTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void RemoveWorkerFromShiftTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void SendRequestTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void RemoveWorkerFromAllHisShiftsTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void ApproveRequestTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void RemoveAllRequestsForAWorkerTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void DenyRequestTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void RemoveRequestTest()
+        //    {
+        //        Assert.Fail();
+        //    }
 
-    //    [Test()]
-    //    public void FindSingleShiftTest()
-    //    {
-    //        Assert.Fail();
-    //    }
+        //    [Test()]
+        //    public void ViewShiftInformationTest()
+        //    {
+        //        Assert.Fail();
+        //    }
+
+        //    [Test()]
+        //    public void ViewMyShiftsTest()
+        //    {
+        //        Assert.Fail();
+        //    }
+
+        //    [Test()]
+        //    public void SendRequestTest()
+        //    {
+        //        Assert.Fail();
+        //    }
+
+        //    [Test()]
+        //    public void ApproveRequestTest()
+        //    {
+        //        Assert.Fail();
+        //    }
+
+        //    [Test()]
+        //    public void DenyRequestTest()
+        //    {
+        //        Assert.Fail();
+        //    }
+
+        //    [Test()]
+        //    public void FindSingleShiftTest()
+        //    {
+        //        Assert.Fail();
+        //    }
     }
 }
