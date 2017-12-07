18e164beac2c320e7782c575df39ce006aad83d1
casper-lp <cpede16@student.aau.dk>
2017-12-07 14:48:08 +0100

    Henter id fra databasen men er ikke lavet i databasen

--- VolunteerPrototype/VolunteerPrototype/User.cs
+++ VolunteerPrototype/VolunteerPrototype/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
 
 namespace VolunteerPrototype
 {
@@ -12,6 +13,25 @@ namespace VolunteerPrototype
         private string _name;
         public string Name { get { return _name; } }
         public int VolunteerId { get { return _volunteerId; } }
+        public User(string email, string hashedPassword)
+        {
+            _name = email;
+           // GetVolunteerID(email, hashedPassword);
+        }
 
+        private void GetVolunteerID(string email, string hashedPassword)
+        {
+
+            string querry = $"SELECT VolunteerId FROM UserTable WHERE email = '{email}' AND password = '{hashedPassword}';";
+
+            var nhehe = new System.Data.SqlClient.SqlConnection(VolunteerSystem.Database2.DBConnection.ConnectionString);
+            nhehe.Open();
+            var hehehe = nhehe.CreateCommand();
+            hehehe.CommandText = querry;
+            var reader = hehehe.ExecuteReader();
+            _volunteerId = (int)reader[0];
+            nhehe.Close();
+
+        }
     }
 }
