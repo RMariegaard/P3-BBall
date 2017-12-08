5b4693c662d53aefa8601f805b72ac6aaa2c9224
casper-lp <cpede16@student.aau.dk>
2017-12-08 12:07:17 +0100

    Hash

--- KaspersTing/VolunteerSystem/VolunteerSystem/Function/AccountController.cs
+++ KaspersTing/VolunteerSystem/VolunteerSystem/Function/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace VolunteerSystem
 {
@@ -22,6 +23,19 @@ namespace VolunteerSystem
         public static void ForgotPassword()
         {
             throw new NotImplementedException();
+        }
+
+        public static object GetHash(string password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+            var hashAlg = SHA256Managed.Create();
+            byte[] hash = hashAlg.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
         }
     }
 }
