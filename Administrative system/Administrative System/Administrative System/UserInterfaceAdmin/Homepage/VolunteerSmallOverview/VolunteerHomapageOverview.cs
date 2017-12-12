6eb4e45916871d085a8c86fa66177d7662954169
casper-lp <cpede16@student.aau.dk>
2017-12-12 12:14:07 +0100

    gotta get commits
    Jeg har opdateret homepage så der står volunteer istedet for view a single volunter

--- Administrative system/Administrative System/Administrative System/UserInterfaceAdmin/Homepage/VolunteerSmallOverview/VolunteerHomapageOverview.cs
+++ Administrative system/Administrative System/Administrative System/UserInterfaceAdmin/Homepage/VolunteerSmallOverview/VolunteerHomapageOverview.cs
@@ -33,7 +33,7 @@ namespace VolunteerSystem.UserInterfaceAdmin.Homepage.VolunteerSmallOverview
             _volunteerHomapageOverviewMainPanel.Size = size;
 
             titleTopLabel.Location = new Point(10, 0);
-            titleTopLabel.Text = "View a single volunteer";
+            titleTopLabel.Text = "Volunteer";
             titleTopLabel.Font = new Font("Arial", 24, FontStyle.Bold);
             titleTopLabel.AutoSize = true;
 
